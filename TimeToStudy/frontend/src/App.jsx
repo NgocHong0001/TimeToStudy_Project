6d6f8f0b9b72b97570c1ca2c86dfb3bc91109f9f
Mi <ngoc.hong0001@stud.hkr.se>
2025-04-30 04:05:28 +0200

    Started with base Layout still in process, created routes home & about, goodnight

--- TimeToStudy/frontend/src/App.jsx
+++ TimeToStudy/frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-//import './App.css'
+//import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'; //enable page routing
+import Layout from './Layout';
+import Home from './pages/Home';
+import About from './pages/About';
 
-function App() {
+export default function App() {
   return (
-   
-      <div>
-        <h1>Welcome to Time To Study!</h1>
-      
-    </div>
-
+    <Routes>
+      <Route path="/" element={<Layout />}> 
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
   )
 }
-
-export default App
---
+++ TimeToStudy/frontend/src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="page">
+      <h1>About Us</h1>
+      <p>We help students manage their deadlines and study time.</p>
+    </div>
+  );
+}
